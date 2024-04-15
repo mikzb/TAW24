4f8e711c66ce4512e70e769c6e740fd004ed4a3e
klnstprx <ignacy.borzestowski@gmail.com>
2024-04-15 11:17:22 +0200

    spring project

---
+++ src/main/java/es/uma/taw24/Taw24Application.java
@@ -0,0 +1,13 @@
+package es.uma.taw24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taw24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taw24Application.class, args);
+    }
+
+}
---
+++ src/test/java/es/uma/taw24/Taw24ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uma.taw24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taw24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
